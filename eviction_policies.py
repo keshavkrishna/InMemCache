67d54218e59e4f03ffc7520c87fc0297e94409ac
Keshav Krishna <57027202+keshavkrishna@users.noreply.github.com>
2025-06-07 19:58:07 +0530

    Update eviction_policies.py

--- eviction_policies.py
+++ eviction_policies.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import Generic, TypeVar
 from collections import OrderedDict
+import random
 
 K = TypeVar('K')
 
@@ -56,3 +57,36 @@ class LIFOEvictionPolicy(EvictionPolicy[K]):
 
     def evict(self) -> K:
         return self.stack.pop()
+
+class RandomEvictionPolicy(EvictionPolicy[K]):
+    def __init__(self):
+        self.items = set()
+
+    def add(self, key: K) -> None:
+        self.items.add(key)
+
+    def remove(self, key: K) -> None:
+        self.items.discard(key)
+
+    def evict(self) -> K:
+        if not self.items:
+            raise IndexError("Eviction called on empty policy.")
+        key = random.choice(list(self.items))
+        self.items.remove(key)
+        return key
+
+class MRUEvictionPolicy(EvictionPolicy[K]):
+    def __init__(self):
+        self.order = OrderedDict()
+
+    def add(self, key: K) -> None:
+        if key in self.order:
+            del self.order[key]
+        self.order[key] = None
+
+    def remove(self, key: K) -> None:
+        self.order.pop(key, None)
+
+    def evict(self) -> K:
+        return self.order.popitem(last=True)[0]
+
